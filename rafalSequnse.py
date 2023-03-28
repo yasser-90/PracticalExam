5f5917d251a49ed72f7c31674ade2103f4390c14
Rafal <rafaltheysb.22.9@gmail.com>
2023-03-28 11:41:27 +0300

    sequnse Solve

---
+++ rafalSequnse.py
@@ -0,0 +1,26 @@
+def fact (fact) :
+    facto = int(input('enter number : '))
+    facts= 1
+    for facts in range(fact,1,-1):
+        facts = fact*facto
+        print(facts)
+
+def pwer(power):
+    x = int(input('enter number of x : '))
+    power=1
+    for power in x :
+        power=x**fact
+        print(power)
+
+print('s= ',fact / pow)
+
+        
+
+
+
+    
+   
+   
+
+
+
