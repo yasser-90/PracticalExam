fe0ef0ed1cbb8ad883260f8acb4a1f5953f0a404
Sarah Mekdad <sarahalmawla321@gmail.com>
2023-03-28 10:52:28 +0300

    solve the problem

---
+++ function.py
@@ -0,0 +1,8 @@
+def factorial(num):
+    f = 1
+    for i in range(1,num+1):
+        f *= i
+    return f
+
+def power(n,p):
+    return n**p
--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,13 @@
-n=input('n=')
+from function import *
+
+n=int(input('n= '))
+x=int(input('x= '))
+
+sum = 0
+sign = 1
+
+for i in range(1,n+1,2):
+    sum += ( factorial(i) / power(x,i) ) * sign
+    sign *= -1
+
+print(sum)
