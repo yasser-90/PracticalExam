95d7a5ea45d2899dea358d07b563c9a22097adf8
abd <abddlal123123m@gmail.com>
2023-03-28 10:51:11 +0300

    first

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,21 @@
-n=input('n=')
+
+
+def fac(x):
+
+    if x == 1:
+        return 1
+
+    else:
+        return x *fac(x-1)
+
+def poww(x,s):
+    return x**s
+n = int(input('n='))
+
+g= 1
+sum = 0
+for i in range(1,n+1,2):
+    sum = sum + (fac(i)/poww(i,i))*g
+    g=g*-1
+
+print(sum)
