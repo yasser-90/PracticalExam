d2dda30205abdb21e55234d624a3e5e12ed2a130
AbeerAkramS4A <abeerakram978@gamil.com>
2023-03-28 11:22:09 +0300

    Changes to be committed:
    	modified:   mainpro.py
    	new file:   op.py

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,20 @@
-n=input('n=')
+from op import opriation as opar
+n=int(input('input the range'))
+x=int(input('input number'))
+s=0
+s2=0
+power1=0
+fac1=0
+i=1
+for i in range(n):
+    power1=opar.power(x,i)
+    fac1=opar.fac(i)
+    s2=(fac1/power1)
+    if  s2==0:
+        print ("ZeroDivisionError: division by zero ")
+        break
+    else:
+        s=s+s2
+print(s)
+
+
---
+++ op.py
@@ -0,0 +1,12 @@
+import math
+class opriation:
+    def power(x,y):
+        z=x**y
+        return z
+    def fac(x):
+        y=1
+        y=y*x
+        x=x-1
+        if x!=0:
+            return y
+
