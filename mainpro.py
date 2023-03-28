00539a4a9bde11cdb6ecaab69c157a0a4351456a
shefaa <your email@SAMLECOM>
2023-03-28 11:07:34 +0300

    exam

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,13 @@
+from Operations  import opeartion as oper
 n=input('n=')
+def factorial(n):
+	
+	if n == 0:
+		return 1
+	
+	return n * factorial(n-1)
+    
+def pow(num):
+    print (pow(num))
+   
+
