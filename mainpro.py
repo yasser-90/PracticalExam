f6ddf2020147a1c5fa9b98e244244bace74a4ffa
ALIMOHAMEEDTAHAS4B <ali.cs482@student.uomosul.edu.iq>
2023-03-28 11:30:17 +0300

    Hi

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,28 @@
-n=input('n=')
+import math 
+def fact(n):
+    for i in range(1,n+1,2):
+        # print(i)
+        k = math.factorial(i) 
+        print(k)
+    
+
+def po(x,n):
+    for i in range(1,n+1,2):
+        y = math.pow(x, i)
+        print(y)
+        
+
+
+
+n = int(input('n='))
+x = int(input('x='))
+# s = fact(n) / po(x,n)
+x = -1
+for i in range(n):
+    ser = fact(n)  +  po(x,n) * x
+    print(ser)
+    print(x)
+    x = x *(-1)  
+    
+
+
