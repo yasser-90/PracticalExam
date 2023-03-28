18a181717d9310779797fdad23140f490eaff908
SafaRafidS4A <safa.cs1551@student.uomosul.edu.iq>
2023-03-28 11:22:07 +0300

    add fact function add power function

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,5 @@
-n=input('n=')
+from operation import operations as oper
+n=int(input('n='))
+for num in range(n):
+    result= oper.fact(num)/oper.pow(n,num)
+print(result)
---
+++ operation.py
@@ -0,0 +1,8 @@
+class operations:
+    def fact(x):
+        for num in range(x):
+            y=num*x+(num*x-num)
+        return y
+    def pow(m,n):
+        z=m**n
+        return z
