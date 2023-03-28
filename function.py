e0f0f4b0569de74fda6cde4532a56d4a5d5a34a4
HashimQaysS4A <hashim.cs502@student.uomosul.edu.iq>
2023-03-28 01:27:33 -0700

    Hashim

---
+++ function.py
@@ -0,0 +1,14 @@
+#functions
+def fact(n):
+ while True:
+   n=input('Enter The number :- ')
+   for n in range(n):
+    n=n*1
+ return n
+def power(x,n):
+ while True:
+   n=input('Enter the number :-')
+   x=input('Enter the power :-')
+   for x in range(n):
+    x=x*n
+ return x
--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,6 @@
-n=input('n=')
+import math
+from function import fact
+from function import power
+n =input('Enter the sequence  : -')
+print(n)
+print(x)
