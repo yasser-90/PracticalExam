6cb7f7eb073f7d4b0ae85221d3862d3c3d7ad8b8
rany <ranyaalhamdany@gmail.com>
2023-03-28 11:29:16 +0500

    result exam

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,24 @@
 n=input('n=')
+
+def fact(n):
+    for m in n:
+        y=m*n
+        return(fact)
+
+
+def pow(n):
+    for w in n:
+        p=x^w
+        return(pow)
+    
+    
+def resul():
+    for s in n:
+        r=fact/pow+s
+        return(resul)
+    
+
+
+
+
+    print(resul)
