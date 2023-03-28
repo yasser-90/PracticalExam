3de8e8e85cd9b38ed52886145e1244a8385d5803
ALIMOHAMEEDTAHAS4B <ali.cs482@student.uomosul.edu.iq>
2023-03-28 11:34:15 +0300

    Hellooo

--- mainpro.py
+++ mainpro.py
@@ -1,28 +1,12 @@
-import math 
-def fact(n):
-    for i in range(1,n+1,2):
-        # print(i)
-        k = math.factorial(i) 
-        print(k)
-    
-
-def po(x,n):
-    for i in range(1,n+1,2):
-        y = math.pow(x, i)
-        print(y)
-        
-
-
-
+from new import fact,po 
 n = int(input('n='))
 x = int(input('x='))
-# s = fact(n) / po(x,n)
-x = -1
-for i in range(n):
-    ser = fact(n)  +  po(x,n) * x
-    print(ser)
-    print(x)
-    x = x *(-1)  
-    
+sum = 1
 
+for i in range(n):
+    ser = (fact(n)/po(x, n))*sum  
+    print(ser)
+    # print(sum)
+    sum = sum *(-1)  
+    
 
---
+++ new.py
@@ -0,0 +1,14 @@
+import math 
+def fact(n):
+    for i in range(1,n+1,2):
+        # print(i)
+        k = math.factorial(i) 
+        print(k)
+        
+    
+
+def po(x,n):
+    for i in range(1,n+1,2):
+        y = math.pow(x, i)
+        print(y)
+        
