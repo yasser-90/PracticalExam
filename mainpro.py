ee94add085cd87a07b5fb7a9755bf575217c9c95
TebaRaadS4B <tebaalabadi442@gmail.com>
2023-03-28 11:32:09 +0300

    solve

--- mainpro.py
+++ mainpro.py
@@ -1,9 +1,10 @@
-file1 =open('')
+file1 =open('C:\Users\user\Desktop\PracticalExam-main\PracticalExam\t')
 Lines = file1.readlines()
    n=input('n=')
 for line in Lines:
     list=[line]
     s=(1:2:n)
+ 
  
 s=s/x**1
 print("s")
