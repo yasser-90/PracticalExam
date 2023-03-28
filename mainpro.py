13a6ddbe0b5c6d08159646d9f9ae6bfe93075e4c
rusolosamaS4A <rusolazobak@gmail.com>
2023-03-28 11:23:58 +0300

    exam

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,14 @@
+from python import factorial as fact
+def factorial():
 n=input('n=')
+fact = 1
+for i in range(1,n+1):
+fact= fact * i
+def power():
+    po=1
+    for i in range (1,n+1):
+        po=x^2
+    
+        s=!1/x^1 - !3/x^3 + !5/x^5 +!n/x^n
+     print(s)
+
