14f561a096254aa7fe37dc2de1a16bceb4d52cb0
Fatima AlKhayyat <fs8897679@gmail.com>
2023-03-28 11:39:18 +0300

    answer

---
+++ functions.py
@@ -0,0 +1,8 @@
+def powerMethod(num2, num4):
+    s2 = num2 ^ num4
+    return(s2)
+def factorialMethod(num,num3):
+    s=0
+    for ind in range(1,num):
+        s = s + (num / num3)
+    return(s)
