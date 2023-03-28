ea29fda3594374d87100cd15d0cbddb6a9267edd
safaabd <safa.alkaddo@gmail.com>
2023-03-28 11:23:01 +0300

    first

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,24 @@
-n=input('n=')
+with open file("fille.md","r")as content:
+        content=file.read("fille.md")
+
+    def factorial(num):
+   
+        f=1 
+        for i in num:
+            result= f*i
+            f=f+1
+        
+        return(result)
+    
+    
+    
+    def powweer(num1,num2):
+        r=num1^num2
+        
+        return (r)
+    
+    
+factorial(3)
+powweer(2,2)
+
+        
