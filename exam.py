f64f8dfa5c48f0adac3f679fee6feea86ccb4ef7
zahraa_ghofran_teba_3 <zahraa.cs490@student.uomosul.edu.iq>
2023-03-28 11:42:36 +0300

    exam

---
+++ exam.py
@@ -0,0 +1,29 @@
+def fact(num):
+
+    factorail=1
+
+    num=input('Enter th numer')
+    if num >= 1
+      for i in range(1,num+1)
+
+        factorail= factorail *i
+
+print(num)
+
+def power(num):
+   power('a','b')
+   for i in a
+    pow(1,3,5)
+    print(num)
+     
+    
+    
+
+
+   
+
+   
+
+
+
+
