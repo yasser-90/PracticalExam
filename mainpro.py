d3f4d2667dc52a778d27202e3b715dbe114974f6
ghofran <ghfran.cs496@student.uomosul.edu.iq>
2023-03-28 17:53:14 -0700

    factorial

--- mainpro.py
+++ mainpro.py
@@ -1,1 +1,12 @@
 n=input('n=')
+def fact1():
+    n = int (input ("Please enter a whole number: ") )
+    fact = 1
+    for factor in range (n,1,-1):
+        fact = fact * factor
+        
+        print ("The factorial of", n, "is", fact)
+        #main ()
+def power(num):
+    return num^num
+print(power(num=))
